163b9511ac7a0d6dab63d66cbc106895f7f36cdc
majam <majamroczek.pl@gmail.com>
2025-10-22 17:42:18 +0200

    fix: make navbar responsive

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Routes, Route} from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react' 
+import Navbar from './components/navbar'
+import HeroSection from './components/hero-section'
+import CocktailsList from './components/cocktails-list'
+import Description from './components/description'
+import lottie from 'lottie-web'
+import { useCocktailsByCategory } from './hooks/use-cocktails'
+import { useCategories } from './hooks/use-categories'
+export default function App() {
+   const { categories } = useCategories();
+   const { loading, cocktailsByCategory} = useCocktailsByCategory(categories);
+    const container = useRef<HTMLDivElement | null>(null);
+    const [anim, setAnim] = useState(true)
+
+  useEffect(()=>{
+    if (!container.current) return; 
+
+     const animation = lottie.loadAnimation({
+      container: container?.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: "/Cheers!.json"
+    });
+    const timer = setTimeout(() => setAnim(false), 3000)
+
+    return () => {animation.destroy(); clearTimeout(timer)} ;
+  }
+    ,[])
+
+    const showAnimation = loading || anim
+
+  return (
+    <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {showAnimation ? (<div className="fixed inset-0 flex items-center justify-center bg-black">
+ <div ref={container} className='w-1/4'/>
+</div>) : (<> <Navbar />
+              <HeroSection />
+              <CocktailsList categories={categories} cocktailsByCategory={cocktailsByCategory} loading={loading}/> </> )}
+            </>
+          }
+        />
+        <Route path="/description/:id" element={<Description />} />
+    </Routes>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './globals.css'
-import App from './page.tsx'
-
+import App from './App'
+import {BrowserRouter} from "react-router-dom"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
