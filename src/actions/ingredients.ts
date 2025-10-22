4d88410987f3cc4243bb7b339a2e73b5418e33d3
majam <majamroczek.pl@gmail.com>
2025-10-22 17:40:54 +0200

    feat: create description with ingredients

---
+++ src/actions/ingredients.ts
@@ -0,0 +1,67 @@
+export interface Ingredient {
+  id: number;
+  name: string;
+  description: string;
+  alcohol: boolean;
+  type: string;
+  percentage: number;
+  imageUrl: string;
+}
+
+export interface Details {
+  id: number;
+  name: string;
+  category: string;
+  description: string;
+  glass: string;
+  instructions: string;
+  imageUrl: string;
+  alcoholic: boolean;
+  ingredients: Ingredient[];
+}
+
+export async function getCocktailDetails(id: number): Promise<Details | null> {
+  const url = import.meta.env.VITE_COCKTAILS_API;
+  try {
+    const response = await fetch(`${url}${id}`);
+    const result = await response.json();
+    console.log("result", result)
+
+    if (!response.ok) {
+      console.error("Failed to fetch cocktail details");
+      return null;
+    }
+
+    const data = result.data;
+
+    if (!data) {
+      console.error("No data returned from API");
+      return null;
+    }
+
+    const details: Details = {
+      id: data.id,
+      name: data.name,
+      category: data.category,
+      description: data.description,
+      glass: data.glass,
+      instructions: data.instructions,
+      imageUrl: data.imageUrl,
+      alcoholic: data.alcoholic,
+      ingredients: data.ingredients.map((ing: any) => ({
+        id: ing.id,
+        name: ing.name,
+        description: ing.description,
+        alcohol: ing.alcohol,
+        type: ing.type,
+        percentage: ing.percentage,
+        imageUrl: ing.imageUrl,
+      })),
+    };
+
+    return details;
+  } catch (err) {
+    console.error("Query error:", err);
+    return null;
+  }
+}
---
+++ src/components/ingredients.tsx
@@ -0,0 +1,27 @@
+import { type Details } from "../actions/ingredients"
+import "../globals.css"
+interface IngredientsProps {
+    details: Details | null;
+}
+
+export default function Ingredients({details}:IngredientsProps){
+    return (
+        <div className="w-full py-10">
+            <h1 className='text-gray-900 text-6xl font-medium md:mx-30 sm:mx-20 my-10 animation-appear'>Ingredients</h1>
+            {details?.ingredients.map((data, idx)=>(
+               <div key={idx} className="mx-6 my-10 flex flex-col md:flex-row items-start gap-8 md:mx-30 md:my-15">
+                {data.imageUrl != null && <div className="p-5 rounded-2xl section-color sm:w-full md:w-1/3 animation-appear">
+                    <img src={data.imageUrl} alt="ingredient img" className="w-full"/>
+                </div>}
+                {data.imageUrl == null && <div className="md:w-1/3 ">
+                </div>}
+                <div className="p-2 rounded-2xl section-color sm:w-full md:w-2/3 animation-appear">
+                    <h2 className="text-gray-900 text-2xl font-medium my-10 text-center">{data.name}</h2>
+                    {data.description != null &&<p className="p-10 text-justify">{data.description}</p>}
+                </div>
+                </div>
+            )
+            )}
+        </div>
+    )
+}
